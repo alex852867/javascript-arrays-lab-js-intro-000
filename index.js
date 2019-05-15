57579f84d353658543468d63291d8fa591dca2d8
Alex East <alexeastltd@gmail.com>
2019-05-15 23:14:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens]
 }
-
+function removeLastKitten(name) {
+  return kittens.slice([0,1])
+}
 
 // Add your functions and code here
